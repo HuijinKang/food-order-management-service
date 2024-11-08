89bb0360f2befd5278b4f90e4f4339d60d472dd1
rua0704 <rua0704@gmail.com>
2024-11-08 16:26:57 +0900

    feat - controller 구현 완료

--- src/main/java/org/sparta/foodordermanagementservice/controller/AuthController.java
+++ src/main/java/org/sparta/foodordermanagementservice/controller/AuthController.java
@@ -3,7 +3,10 @@ package org.sparta.foodordermanagementservice.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sparta.foodordermanagementservice.common.ApiResponse;
+import org.sparta.foodordermanagementservice.common.exeption.CustomException;
+import org.sparta.foodordermanagementservice.common.exeption.ErrorCode;
 import org.sparta.foodordermanagementservice.dto.request.SignupRequestDTO;
+import org.sparta.foodordermanagementservice.entity.UserRole;
 import org.sparta.foodordermanagementservice.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,13 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ApiResponse<Void> signup(@Valid @RequestBody SignupRequestDTO request) {
+        UserRole userRole = request.getUserRole();
+
+        if(userRole != UserRole.CUSTOMER && userRole != UserRole.OWNER) {
+            throw new CustomException(ErrorCode.WRONG_ROLE);
+        }
+
+        authService.signup(request);
 
         return ApiResponse.ofSuccess(null);
     }
