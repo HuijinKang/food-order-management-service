6b7e17bfbfe6d45252ce22f617eeef42e84904cb
rua0704 <rua0704@gmail.com>
2024-11-11 12:19:55 +0900

    feat - User에 Timestamped 상속 & EnableJpaAuditing 적용

--- src/main/java/org/sparta/foodordermanagementservice/entity/User.java
+++ src/main/java/org/sparta/foodordermanagementservice/entity/User.java
@@ -35,7 +35,8 @@ public class User extends Timestamped{
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private UserStatus status;
+    @Builder.Default
+    private UserStatus status = UserStatus.ACTIVE;
 
     @Column(nullable = false, unique = true, length = 100)
     private String nickname;
