f4cc5b358567e4b85cbf2142a75eae3be473d936
김성용 <tjddyd1565@naver.com>
2024-11-13 02:12:40 +0900

    refac - PageSizeRule.isValid-> PageSizeRule.validate 변경

--- src/main/java/org/sparta/foodordermanagementservice/common/PageSizeRule.java
+++ src/main/java/org/sparta/foodordermanagementservice/common/PageSizeRule.java
@@ -2,7 +2,7 @@ package org.sparta.foodordermanagementservice.common;
 
 public class PageSizeRule {
     public static final int DEFAULT_PAGE_SIZE = 10;
-    public static boolean isPageSizeValid(int pageSize) {
+    public static boolean validate(int pageSize) {
         return pageSize == 10 || pageSize == 30 || pageSize == 50;
     }
 
