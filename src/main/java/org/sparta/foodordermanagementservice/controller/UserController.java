43a08ade6b9e20d4128cf4ef7ba13ab5c49b22d5
rua0704 <rua0704@gmail.com>
2024-11-13 22:53:24 +0900

    feat - 컨트롤러 코드 작성

--- src/main/java/org/sparta/foodordermanagementservice/controller/UserController.java
+++ src/main/java/org/sparta/foodordermanagementservice/controller/UserController.java
@@ -2,12 +2,15 @@ package org.sparta.foodordermanagementservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sparta.foodordermanagementservice.common.ApiResponse;
+import org.sparta.foodordermanagementservice.common.exeption.CustomException;
+import org.sparta.foodordermanagementservice.common.exeption.ErrorCode;
 import org.sparta.foodordermanagementservice.dto.UserDTO;
 import org.sparta.foodordermanagementservice.entity.UserRole;
 import org.sparta.foodordermanagementservice.service.UserService;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,7 +31,22 @@ public class UserController {
     @PutMapping("/{username}")
     public ApiResponse<Void> updateUser(@PathVariable("username") String username,
                                            @AuthenticationPrincipal UserDetails userDetails) {
+        if(username.equals(userDetails.getUsername())
+                || hasManagerRole(userDetails)
+                || hasMasterRole(userDetails)) {
+            userService.deleteUser(username);
+        } else {
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
 
         return ApiResponse.ofSuccess(null);
+    }
+
+    private static boolean hasMasterRole(UserDetails userDetails) {
+        return userDetails.getAuthorities().stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(UserRole.Authority.MANAGER));
+    }
+
+    private static boolean hasManagerRole(UserDetails userDetails) {
+        return userDetails.getAuthorities().stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(UserRole.Authority.MANAGER));
     }
 }
