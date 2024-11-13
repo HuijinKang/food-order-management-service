c92adfa46981334fc37fd98ffecd8aece138e315
rua0704 <rua0704@gmail.com>
2024-11-13 17:58:33 +0900

    fix - ObjectMapper 사용시 기본생성자를 찾지 못하는 문제 해결

--- src/main/java/org/sparta/foodordermanagementservice/dto/request/UpdateUserRoleRequestDTO.java
+++ src/main/java/org/sparta/foodordermanagementservice/dto/request/UpdateUserRoleRequestDTO.java
@@ -1,5 +1,6 @@
 package org.sparta.foodordermanagementservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,10 +9,11 @@ import org.sparta.foodordermanagementservice.entity.UserRole;
 @Getter
 public class UpdateUserRoleRequestDTO {
     @NotNull(message = "역할 입력은 필수입니다.")
-    private final UserRole userRole;
+    private UserRole userRole;
 
     @Builder
-    public UpdateUserRoleRequestDTO(@NotNull UserRole userRole) {
+    @JsonCreator
+    public UpdateUserRoleRequestDTO(UserRole userRole) {
         this.userRole = userRole;
     }
 }
