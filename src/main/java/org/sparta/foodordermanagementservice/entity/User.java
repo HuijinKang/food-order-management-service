e4320c66046defef7c48479a4850c95e581d5752
rua0704 <rua0704@gmail.com>
2024-11-13 12:00:01 +0900

    fix - 유저 정보 조회에서 비밀번호 제거

--- src/main/java/org/sparta/foodordermanagementservice/entity/User.java
+++ src/main/java/org/sparta/foodordermanagementservice/entity/User.java
@@ -41,7 +41,7 @@ public class User extends Timestamped{
     @Column(nullable = false, unique = true, length = 100)
     private String nickname;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false, unique = true, length = 255)
     private String email;
 
     @Enumerated(EnumType.STRING)
