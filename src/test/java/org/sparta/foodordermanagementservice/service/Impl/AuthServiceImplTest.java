a8509f9c080a0fbdc8f02df98046f985f5e72191
rua0704 <rua0704@gmail.com>
2024-11-14 11:00:13 +0900

    test - 변수명 컨벤션 적용

--- src/test/java/org/sparta/foodordermanagementservice/service/Impl/AuthServiceImplTest.java
+++ src/test/java/org/sparta/foodordermanagementservice/service/Impl/AuthServiceImplTest.java
@@ -53,14 +53,14 @@ class AuthServiceImplTest {
     @BeforeEach
     void setUp() {
          testUser = User.builder()
-                .username("testuser")
-                .password("testpassword")
+                .username("testUser")
+                .password("testPassword")
                 .email("test@email.com")
-                .nickname("testnickname")
+                .nickname("testNickname")
                 .isPublic(true)
                 .userRole(UserRole.CUSTOMER)
-                .createdBy("testuser")
-                .updatedBy("testuser")
+                .createdBy("testUser")
+                .updatedBy("testUser")
                 .build();
         validSignUpRequest = SignupRequestDTO.builder()
                 .username(testUser.getUsername())
@@ -166,7 +166,10 @@ class AuthServiceImplTest {
         when(userRepository.findByUsername(anyString()))
                 .thenReturn(Optional.empty());
 
-        assertThrows(CustomException.class, () -> authService.login(notExistentUser));
+        CustomException exception = assertThrows(CustomException.class, () -> authService.login(notExistentUser));
+
+        assertEquals(ErrorCode.FAIL_LOGIN, exception.getErrorCode());
+
         verify(userRepository, times(1)).findByUsername(anyString());
 
     }
@@ -175,7 +178,7 @@ class AuthServiceImplTest {
     @DisplayName("로그인 실패 - 비밀번호 불일치")
     void loginFailWhenPasswordIsInvalid() {
         LoginRequestDTO IncorrectPasswordUser = LoginRequestDTO.builder()
-                .username("testuser")
+                .username("testUser")
                 .password("incorrectPassword")
                 .build();
 
