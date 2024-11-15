2a81919df4904c90658bc78980bb92a9a20ee7c4
pk_hyun <pkh954533@gmail.com>
2024-11-15 19:54:53 +0900

    fix - 리퀘스트 파람 형식에서 ModelAttribute 로 변경

--- src/main/java/org/sparta/foodordermanagementservice/dto/request/ReviewListRequestDto.java
+++ src/main/java/org/sparta/foodordermanagementservice/dto/request/ReviewListRequestDto.java
@@ -1,10 +1,12 @@
 package org.sparta.foodordermanagementservice.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ReviewListRequestDto {
-    private int pageNumber;
-    private int pageSize;
-    private boolean isAsc;
+    private int pageNumber = 0; // 기본값 설정
+    private int pageSize = 10; // 기본값 설정
+    private boolean isAsc = true;
 }
