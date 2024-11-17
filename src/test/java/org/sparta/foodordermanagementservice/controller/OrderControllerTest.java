57a3e7ce4a10f33316bab2d75d7d1873f4b5c9d3
김성용 <tjddyd1565@naver.com>
2024-11-17 17:13:49 +0900

    fix - 컨트롤러 테스트 필요한 어노테이션

--- src/test/java/org/sparta/foodordermanagementservice/controller/OrderControllerTest.java
+++ src/test/java/org/sparta/foodordermanagementservice/controller/OrderControllerTest.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.sparta.foodordermanagementservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,7 +20,9 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
-
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class})
 class OrderControllerTest {
 
     @MockBean
