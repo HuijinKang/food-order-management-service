62536042f6be6df33677cfa5e06a8266bc3c1fc3
김성용 <tjddyd1565@naver.com>
2024-11-17 17:10:17 +0900

    refac - BaseEntity extend받도록 수정

--- src/main/java/org/sparta/foodordermanagementservice/entity/Payment.java
+++ src/main/java/org/sparta/foodordermanagementservice/entity/Payment.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @Table(name = "p_payment")
-public class Payment {
+public class Payment extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/test/java/org/sparta/foodordermanagementservice/controller/OrderControllerTest.java
+++ src/test/java/org/sparta/foodordermanagementservice/controller/OrderControllerTest.java
@@ -1,13 +1,50 @@
 package org.sparta.foodordermanagementservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.sparta.foodordermanagementservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
 
 class OrderControllerTest {
 
+    @MockBean
+    private OrderService orderService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    void setUp(WebApplicationContext context, RestDocumentationContextProvider provider) {
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(documentationConfiguration(provider))
+                .build();
+    }
+
+
     @Test
+    @WithMockUser(username = "testUser", roles = {"OWNER"})
+    @DisplayName("주문 목록 조회 성공")
     void paginateOrders() {
+
+
     }
 
     @Test
