0afe1d829aedc8866611a0019a7ca70a880fb8cf
rua0704 <rua0704@gmail.com>
2024-11-18 20:18:04 +0900

    fix - rollback

--- src/test/java/org/sparta/foodordermanagementservice/FoodOrderManagementServiceApplicationTests.java
+++ src/test/java/org/sparta/foodordermanagementservice/FoodOrderManagementServiceApplicationTests.java
@@ -5,9 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
-
+@EnableJpaAuditing
 class FoodOrderManagementServiceApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 }
