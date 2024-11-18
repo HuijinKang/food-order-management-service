5e180ae0bfca00847387d4942009ba0a724e273b
Seong yong , Kim <69845367+Soeng-dev@users.noreply.github.com>
2024-11-18 12:38:02 +0900

    [Fix] order - enum값 response반환 관련 디버깅 (#54)
    * fix - rest docs 이름 안겹치게 수정
    
    * fix - enum값 json반환 관련 디버깅

--- src/main/java/org/sparta/foodordermanagementservice/entity/enumerate/OrderStatus.java
+++ src/main/java/org/sparta/foodordermanagementservice/entity/enumerate/OrderStatus.java
@@ -1,5 +1,6 @@
 package org.sparta.foodordermanagementservice.entity.enumerate;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.RequiredArgsConstructor;
 import org.sparta.foodordermanagementservice.dto.request.BaseQueryStringEnum;
 
@@ -16,6 +17,7 @@ public enum OrderStatus implements BaseQueryStringEnum {
     private final String queryString;
 
     @Override
+    @JsonValue
     public String getQueryString() {
         return queryString;
     }
